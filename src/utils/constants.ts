378984ce7178ad1a8a7d506b5ad896819fbd27f1
Camilo <80482255+Sir-M4kU@users.noreply.github.com>
2023-09-22 17:06:41 -0500

    Added links to 'onlyfox.vip'

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,14 @@
+const ONLYFOX = 'https://onlyfox.vip'
+
 export const FOX_SOCIAL_NETWORKS = {
   Twitch: 'https://twitch.tv/anny',
   Twitter: 'https://twitter.com/annytf',
   Discord: 'https://discord.gg/anny',
   Reddit: 'https://www.reddit.com/r/annyfox'
 } as const
+
+export const ONLYFOX_LINKS = {
+  Mega: [`${ONLYFOX}/mega/1`, `${ONLYFOX}/mega/2`],
+  Hics: [`${ONLYFOX}/hics/1`, `${ONLYFOX}/hics/2`],
+  Vods: `${ONLYFOX}/vods`
+}
