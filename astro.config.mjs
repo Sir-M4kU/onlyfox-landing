1584baae92d3aa7bd99271e37f2badc80f8f2a64
Camilo <80482255+Sir-M4kU@users.noreply.github.com>
2023-09-22 20:21:56 -0500

    Deploy on github pages (first try, maybe)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://sir-m4ku.github.io',
+  base: 'onlyfox-landing'
 })
